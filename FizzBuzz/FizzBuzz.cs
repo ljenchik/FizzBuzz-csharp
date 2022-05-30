0d9761ebf1da4c2fb608391c8f255946aa5fc422
ljenchik <pelagenkolena@gmail.com>
2022-05-30 14:58:42 +0100

    Added 11, 13, 17

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -2,11 +2,9 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
-
-
-        public int indexOfB(string[] arr)
+        public int indexOfB(List<String> arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Count; i++)
             {
                 if (arr[i].StartsWith("B"))
                 {
@@ -15,7 +13,38 @@ namespace FizzBuzz
             }
             return -1;
         }
-
+        public static List<String> Splice(List<String> result, int index)
+        {
+            List<String> firstPart = result.GetRange(0, index);
+            Console.WriteLine("firstPart before 'Fezz' is added:");
+            foreach (string word in firstPart) 
+            {
+                Console.WriteLine($"->{word}");
+            }
+            Console.WriteLine("--");
+            firstPart.Add("Fezz");
+            Console.WriteLine("firstPart after 'Fezz' is added:");
+            foreach (string word in firstPart) 
+            {
+                Console.WriteLine($"->{word}");
+            }
+            Console.WriteLine("--");
+            List<String> secondPart = result.GetRange(index, result.Count - index);
+            Console.WriteLine("secondPart:");
+            foreach (string word in secondPart) 
+            {
+                Console.WriteLine($"->{word}");
+            }
+            Console.WriteLine("--");
+            firstPart.AddRange(secondPart);
+            Console.WriteLine("firstPart after secondPart is added:");
+            foreach (string word in firstPart) 
+            {
+                Console.WriteLine($"->{word}");
+            }
+            Console.WriteLine("--");
+            return firstPart;
+        }
 
         public void DoFizzBuzz(int maxNumber)
         {
@@ -33,6 +62,28 @@ namespace FizzBuzz
                 if (i % 7 == 0)
                 {
                     result.Add("Bang");
+                }
+                if (i % 11 == 0)
+                {
+                    result = new List<string>
+                    {
+                        "Bong"
+                    };
+                }
+                if (i % 13 == 0)
+                {
+                    if (indexOfB(result) == -1)
+                    {
+                        result.Add("Fezz");
+                    }
+                    else
+                    {
+                        result = Splice(result, indexOfB(result));
+                    }
+                }
+                if (i % 17 == 0)
+                {
+                    result.Reverse();
                 }
                 if (result.Count == 0)
                 {
