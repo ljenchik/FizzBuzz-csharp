d4eef7c4431260d7ce57b12333a21e300e7cd23a
ljenchik <pelagenkolena@gmail.com>
2022-05-30 12:30:34 +0100

    Added division by 3 and 5

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public void DoFizzBuzz(int maxNumber)
+        {
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0) 
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
+
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,8 +2,21 @@
 
 namespace FizzBuzz {
     public class Program {
-        public static void Main(string[] args) {
-            Console.WriteLine("Hello World");
+        public static void Main(string[] args) 
+        {
+            int maxNumber;
+            if (args.Length == 0) 
+            {
+                maxNumber = 100;
+            }
+            else 
+            {
+                maxNumber=int.Parse(args[0]);
+            }
+
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.DoFizzBuzz(maxNumber);
+            
         }
     }
 }
